f96b4bf9a4771480bc9f5c112b9366278a89182b
Vaishnavi Bakare <vaishnavibakare4@gmail.com>
2023-06-11 19:48:40 +0530

    first project

--- src_old/app/app.component.spec.ts
+++ src_old/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'RestaurentApp'`, () => {
+  it(`should have as title 'Restaurant'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('RestaurentApp');
+    expect(app.title).toEqual('Restaurant');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('RestaurentApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Restaurant app is running!');
   });
 });
